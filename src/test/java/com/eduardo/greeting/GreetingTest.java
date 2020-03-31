f4b80cf718c18e99f3ba053348c000123fa40d68
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2020-03-31 15:37:24 -0400

    Criei alguns simples casos de testes unitários

---
+++ src/test/java/com/eduardo/greeting/GreetingTest.java
@@ -0,0 +1,40 @@
+package com.eduardo.greeting;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * @author EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
+ *
+ */
+public class GreetingTest {
+    private Greeting greetingResponder;
+
+    public GreetingTest() {
+        this.greetingResponder = new Greeting();
+    }
+
+    @Test
+    public void shouldAnswerWithGoodMorning() {
+        String greeting = this.greetingResponder.getGreeting(7);
+        assertEquals("Good Morning", greeting);
+    }
+
+    @Test
+    public void shouldAnswerWithGoodAfternoon() {
+        String greeting = this.greetingResponder.getGreeting(13);
+        assertEquals("Good Afternoon", greeting);
+    }
+
+    @Test
+    public void shouldAnswerWithGoodNight() {
+        String greeting = this.greetingResponder.getGreeting(22);
+        assertEquals("Good Night", greeting);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldAnswerWithIllegalArgumentException() {
+        this.greetingResponder.getGreeting(25);
+    }
+}
